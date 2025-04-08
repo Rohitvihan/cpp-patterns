d5042716eb37b137bb3ea1746b884f2cc04c12b2
Rohit vihan k <167102294+Rohitvihan@users.noreply.github.com>
2025-04-08 18:47:52 +0530

    Add files via upload

---
+++ pattern-8.cpp
@@ -0,0 +1,37 @@
+to print pattern:-
+                    ***********
+                     *********
+                      *******
+                       *****
+                        ***
+                         *
+#include<bits/stdc++.h>
+using namespace std;
+void pattern_7(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            cout << " ";
+        }
+        for(int j=0; j<2*(n-i)-1; j++)
+        {
+            cout << "*";
+        }
+         for(int j=0; j<i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    pattern_7(n);
+    return 0;
+}
+
